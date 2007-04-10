64e73e0d1eaab6af6b8c7c1baa3fa9d5c179b5da
root <root@848dde88-9e2b-0410-a297-c33dff92dd3a>
2007-04-10 10:55:53 +0000

    Update of QCA

--- main/synapse-im/third-party/qca-sasl/qca-sasl.cpp
+++ main/synapse-im/third-party/qca-sasl/qca-sasl.cpp
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
 
@@ -41,8 +41,10 @@ namespace saslQCAPlugin {
 class saslProvider : public Provider
 {
 public:
+	saslProvider();
 	void init();
 	~saslProvider();
+        int version() const;
 	QString name() const;
 	QString credit() const;
 	QStringList features() const;
@@ -358,7 +360,7 @@ private:
 	void clientTryAgain()
 	{
 		result_haveClientInit = false;
-		
+
 		if(step == 0) {
 			const char *clientout, *m;
 			unsigned int clientoutlen;
@@ -842,6 +844,12 @@ public:
 // saslProvider
 //----------------------------------------------------------------------------
 
+saslProvider::saslProvider()
+	: Provider()
+{
+	init();
+}
+
 void saslProvider::init()
 {
 	client_init = false;
@@ -854,9 +862,14 @@ saslProvider::~saslProvider()
 		sasl_done();
 }
 
+int saslProvider::version() const
+{
+        return QCA_VERSION;
+}
+
 QString saslProvider::name() const
 {
-	return "qca-sasl";
+	return "qca-cyrus-sasl";
 }
 
 QString saslProvider::credit() const
@@ -898,5 +911,5 @@ public:
 
 #include "qca-sasl.moc"
 
-Q_EXPORT_PLUGIN2(qca_sasl, saslPlugin);
+Q_EXPORT_PLUGIN2(qca_sasl, saslPlugin)
 
