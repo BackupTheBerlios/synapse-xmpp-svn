0c3c3e07887cd91441c977e7ca520250a50b894d
hantu <hantu@848dde88-9e2b-0410-a297-c33dff92dd3a>
2007-06-17 08:30:20 +0000

    Version bump.

--- main/synapse-im/src/applicationinfo.cpp
+++ main/synapse-im/src/applicationinfo.cpp
@@ -26,10 +26,10 @@
 // Should also be overridable through an optional file.
 
 #define PROG_NAME "Synapse IM"
-#define PROG_VERSION "0.3 \"alfa\"" " (" __DATE__ ")"; //CVS Builds are dated
+#define PROG_VERSION "0.4 \"alfa 2\"" " (" __DATE__ ")"; //CVS Builds are dated
 //#define PROG_VERSION "0.11-beta5";
 #define PROG_CAPS_NODE "http://psi-im.org/caps";
-#define PROG_CAPS_VERSION "0.3-dev-rev1";
+#define PROG_CAPS_VERSION "0.4-dev-rev1";
 #define PROG_OPTIONS_NS "http://psi-im.org/options";
 #define PROG_STORAGE_NS "http://psi-im.org/storage";
 
