d0e59110c0f6c8d265ae05cce7592afbfdde8bc1
hantu <hantu@848dde88-9e2b-0410-a297-c33dff92dd3a>
2007-08-23 17:55:03 +0000

    Fix for contact background color.

--- main/synapse-im/src/SIMContactList/SIMContactName.cpp
+++ main/synapse-im/src/SIMContactList/SIMContactName.cpp
@@ -50,6 +50,9 @@ void SIMContactName::paint(QPainter *painter, const QRect &rect, const QPalette 
 
 	QSize size = v_rt->size().toSize();
 	int h = (rect.height() - size.height())/2;
+	QBrush *paper = new QBrush(option.color[cListBack], Qt::SolidPattern);
+	painter->fillRect( rect.x(), rect.y(), rect.width(), rect.height(), *paper );
+
 	painter->translate(rect.x() + 2, rect.y() + h);
 
 	QAbstractTextDocumentLayout *layout = v_rt->documentLayout();
@@ -57,10 +60,10 @@ void SIMContactName::paint(QPainter *painter, const QRect &rect, const QPalette 
 	
 	context.palette = palette;
 	context.palette.setColor(QPalette::Text, color_);
-
+	
 	layout->draw(painter, context);
 
-	//delete v_rt;
+	delete paper;
 
 	painter->restore();
 }
